5420369d72f7951d1a0a6f52913f808f1fa010bc
etxeg <etxeetxe1019@gmail.com>
2024-12-10 18:16:35 +0100

    Enhance file upload form and backend handling with improved UI and file storage logic

--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,45 @@
 var express = require('express');
 var router = express.Router();
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/home/ander/Desktop/WS/express-multer/public/uploads')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.png'
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 1024 * 1024 * 2
+    }
+})
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.redirect('form.html');
 });
 
 router.post('/', upload.single('avatar'), function (req, res, next) {
     console.log(req.file)
     // req.body will hold the text fields, if there were any
-    res.send("Jasota")
+    res.json({
+        izena: req.body.izena,
+        fitxategia: req.file.filename
+    });
 })
 
 
